87fac9e003f62d7c6492c1a25acde121d2dacceb
YakiroYuri <yurilealdacruz@gmail.com>
2024-04-12 10:42:12 -0300

    Modificando html e css

--- polls/urls.py
+++ polls/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('buscar/', views.buscar_item, name='buscar_item'),  # URL para a view de busca
 ] + static(MEDIA_URL, document_root=MEDIA_ROOT)
--- polls/views.py
+++ polls/views.py
@@ -10,3 +10,12 @@ def index(request):
     return render(request, 'index.html', {'dados':dados})
 
 
+def buscar_item(request):
+    query = request.GET.get('q')  # Obtenha o parâmetro 'q' da query string (nome do item a ser buscado)
+    if query:
+        # Faça a busca no banco de dados pelos itens com o nome semelhante ao que foi buscado
+        items = Estoque.objects.filter(nome__icontains=query)
+    else:
+        items = Estoque.objects.all()  # Se nenhum termo de busca foi fornecido, retorne todos os itens
+
+    return render(request, 'index.html', {'dados': items, 'query': query})
