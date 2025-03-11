51195a80b64080e829352d81ff8b858bf8cd9695
Yuri Leal da Cruz <100494057+yurilealdacruz@users.noreply.github.com>
2025-03-11 20:25:32 -0300

    Adicionando modelo

--- polls/forms.py
+++ polls/forms.py
@@ -16,7 +16,7 @@ class EstoqueForm(forms.ModelForm):
 class EstoqueAlmoForm(forms.ModelForm):
     class Meta:
         model = EstoqueAlmo
-        fields = ['nome', 'estoque', 'endereco', 'categoria', 'foto']
+        fields = ['nome', 'estoque', 'modelo', 'endereco', 'categoria', 'foto']
 
 class EstoqueATForm(forms.ModelForm):
     class Meta:
